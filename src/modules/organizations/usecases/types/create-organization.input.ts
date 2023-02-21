79267c7bc31bd8bead712f8a3b13404d84342dfe
Gabriel Moura <mourasb.gabriel@gmail.com>
2023-02-21 18:35:33 -0300

    feat: add validation to create organization mutation

--- src/modules/organizations/usecases/types/create-organization.input.ts
+++ src/modules/organizations/usecases/types/create-organization.input.ts
@@ -1,15 +1,18 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
 
 @InputType()
 export class CreateOrganizationInput {
   @Field()
+  @IsString()
   name: string;
 
   @Field()
+  @IsEmail()
   representantEmail: string;
 
   @Field()
+  @IsOptional()
+  @IsString()
   businessSegment: string;
-
-  locations?: any[];
 }
