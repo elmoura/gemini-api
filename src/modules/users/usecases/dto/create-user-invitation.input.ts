543fa4f8aa982806cdb4f0745aba3514673b0d08
Gabriel Moura <mourasb.gabriel@gmail.com>
2023-02-21 18:34:22 -0300

    feat: add validation to create user invitation mutation

--- src/modules/users/usecases/dto/create-user-invitation.input.ts
+++ src/modules/users/usecases/dto/create-user-invitation.input.ts
@@ -1,11 +1,15 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, Validate } from 'class-validator';
+import { IsObjectId } from '@shared/validations/is-object-id';
 import { IUserInvitationData } from '@modules/users/datasources/user.datasource';
 
 @InputType()
 export class CreateUserInvitationInput implements IUserInvitationData {
   @Field()
+  @Validate(IsObjectId)
   organizationId: string;
 
   @Field()
+  @IsEmail()
   email: string;
 }
